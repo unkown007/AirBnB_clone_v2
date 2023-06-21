0c481f55f86a5cdba79c73d614e18bd1f23761a6
unkown007 <victormatusse00@gmail.com>
2023-06-21 14:14:30 +0000

    fixed all output

--- console.py
+++ console.py
@@ -225,7 +225,6 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
         print_list = []
-
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
             if args not in HBNBCommand.classes:
@@ -237,8 +236,8 @@ class HBNBCommand(cmd.Cmd):
         else:
             for k, v in storage.all().items():
                 print_list.append(str(v))
-
-        print(print_list)
+        if print_list:
+            print(print_list)
 
     def help_all(self):
         """ Help information for the all command """
