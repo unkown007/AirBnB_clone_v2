c6fe22a210b95701e703244ca195452a1ff448c7
unkown007 <victormatusse00@gmail.com>
2023-07-06 17:18:02 +0000

    added file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# generates a .tgz archive from the contents of the web_static folder
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """ pack web_static folder into a .tgz file """
+    dt = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz".format(dt)
+    print("Packing web_static to {}".format(path))
+    local("mkdir versions")
+    file_tar = local("tar -cvzf {} web_static".format(path))
+    if file_tar.succeeded:
+        return path
