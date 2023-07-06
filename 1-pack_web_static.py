068b2b8a2df2280d817b9cbe577acb66bfceb013
unkown007 <victormatusse00@gmail.com>
2023-07-06 18:20:42 +0000

    fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 # generates a .tgz archive from the contents of the web_static folder
-from os import path, makedirs, listdir
+import os
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
     """ pack web_static folder into a .tgz file """
-    if not path.isdir("./versions"):
-        makedirs("./versions")
-    dt = datetime.now().strftime("%Y%m%d%H%M%S")
-    fpath = "versions/web_static_{}.tgz".format(dt)
-    file_tar = local("tar -cvzf {} web_static".format(fpath))
-    files = listdir("./versions")
-    paths = [path.join("./versions", base_name) for base_name in files]
-    if len(paths) == 0:
+    try:
+        dt = datetime.now().strftime("%Y%m%d%H%M%S")
+        fpath = "versions/web_static_{}.tgz".format(dt)
+        if not os.path.exists("versions"):
+            local("mkdir versions")
+        local("tar -cvzf {} web_static".format(fpath))
+        return fpath
+    except Exception:
         return None
-    return max(paths, key=path.getctime)
