5d2801ccaf79ed177eab99932840369bfea93fbb
unkown007 <victormatusse00@gmail.com>
2023-07-06 15:34:17 +0000

    added file

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# sets up web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee -a /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+#sudo chown -hR "ubuntu" "/data"
+sudo sed -i "/server_name _;/a\ \n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}\n" /etc/nginx/sites-available/default
+sudo service nginx restart
